0427a67855ca6a5c7583b083e6f34a6ef5cd3b0f
Andrea Giammarchi <agiammarchi@twitter.com>
2014-04-12 19:09:55 -0700

    automatic test generator

--- test/build/prototypal.node.js
+++ test/build/prototypal.node.js
@@ -28,6 +28,18 @@ module.exports = function(Object){'use strict';
     PROTOTYPE = 'prototype',
     DIRTY = '__proto__',
 
+    bind = Object.bind || function (context) {
+      var callback = this;
+      return function () {
+        return callback.apply(context, arguments);
+      };
+    },
+    defineProperty = Object.defineProperty,
+    defineProperties = Object.defineProperties,
+    getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor,
+    getPrototypeOf = Object.getPrototypeOf,
+    originalCreate = Object.create,
+
     inherited = [
       CONSTRUCTOR,
       'hasOwnProperty',
@@ -45,8 +57,6 @@ module.exports = function(Object){'use strict';
     // hasOwnProperty shortcut
     has = {}[inherited[1]],
 
-    defineProperty = Object.defineProperty,
-
     keys = Object.keys || function (o) {
       var r = [], k;
       for (k in o) {
@@ -63,8 +73,6 @@ module.exports = function(Object){'use strict';
       }
       return r;
     },
-
-    originalCreate = Object.create,
 
     create = originalCreate ?
 
@@ -131,9 +139,36 @@ module.exports = function(Object){'use strict';
         return o;
       }
     ),
-    constructorPrototype = constructor[PROTOTYPE]
+    constructorPrototype = constructor[PROTOTYPE],
+    hasConfigurableBug,
+    hasNotOwnPropertyBug,
+    tmp
     // , unsafeDictionary = DIRTY in {} && DIRTY in create(null)
   ;
+
+  function Bound(callback) {
+    this._ = callback;
+  }
+
+  function Descriptor(object) {
+    this._ = object;
+  }
+
+  function Lazy(callback) {
+    this._ = callback;
+  }
+
+  // simplified method binding, i.e.
+  // node.on('click', obj.onclick);
+  // where onclick is specified as
+  // {onclick:Class.bound(function(){ ... })}
+  function bound(d, k) {
+    var callback = d[k]._;
+    d[k]._ = function () {
+      return bind.call(callback, this);
+    };
+    return lazy(d, k, callback);
+  }
 
   function chain(p) {
     constructor[PROTOTYPE] = p;
@@ -160,24 +195,160 @@ module.exports = function(Object){'use strict';
     return r;
   }
 
+  // defaults settings used for all
+  // native properties descriptors
+  function defaults(value) {
+    return {
+      value: value,
+      enumerable: false,
+      configurable: true,
+      writable: true
+    };
+  }
+
+  // parse descriptors, looking for
+  // functions or lazy/bound properties
+  function descriptify(d, k) {
+    var current = d[k];
+    if(current instanceof Bound) {
+      d[k] = bound(d, k);
+    } else if(current instanceof Descriptor) {
+      d[k] = current._;
+    } else if(current instanceof Lazy) {
+      d[k] = lazy(d, k);
+    } else {
+      d[k] = defaults(current);
+    }
+    return d;
+  }
+
+  function getConstructor(d, process) {
+    return has.call(d, CONSTRUCTOR) ?
+      d[CONSTRUCTOR] :
+      (d[CONSTRUCTOR] = process(function Class() {}));
+  }
+
+  // simplified lazy assignment, i.e.
+  // {method:Class.lazy(function(){
+  //   // lazily assigned only once
+  //   return Math.random();
+  // })}
+  function lazy(d, k, c) {
+    var callback = d[k]._;
+    return {
+      configurable: true,
+      get: function asd() {
+        var self = this, proto = self, desc;
+        // if accessed directly through the Class.prototype,
+        // returns the original callback otherwise it could compromise
+        // all instances based on lazy pattern
+        // if it was a bound method, it will return the original too
+        // the whole aim of this Class.js is to avoid dealing directly
+        // with the constructor, which means only prototype
+        // should have constructor as own property, never instances
+        if (hasOwnProperty.call(self, CONSTRUCTOR)) {
+          return c || callback;
+        }
+        // Android 2.x and webOS 2 bug only
+        if (hasConfigurableBug) {
+          while (!desc && (proto = getPrototypeOf(proto))) {
+            desc = getOwnPropertyDescriptor(proto, k);
+          }
+          delete proto[k];
+        }
+        if (hasNotOwnPropertyBug || !hasOwnProperty.call(self, k)) {
+          // redefine as configurable to be able later on
+          // to delete it again
+          defineProperty(self, k, {
+            configurable: true,
+            value: callback.call(self)
+          });
+        }
+        // still Android 2.x and webOS 2 only
+        if (hasConfigurableBug) {
+          defineProperty(proto, k, desc);
+        }
+        return self[k];
+      }
+    };
+  }
+
+  if (getOwnPropertyDescriptor) {
+    // Android 2.x and webOS 2 bug only
+    tmp = originalCreate(defineProperty({},'_',{get:function(){
+      ++i; // you wouldn't believe this ... IE9 Mobile ... >
+      return defineProperty(this,'_',{value:false})._;
+    }}));
+    try {
+      hasConfigurableBug = tmp._ || tmp._;
+    } catch(e) {
+      hasConfigurableBug = true;
+    }
+    hasNotOwnPropertyBug = 1 === i; // < ... seriously !!!
+  }
+
+  function Class(proto, descriptors) {
+    var
+      extending = descriptors != null,
+      d = extending ? descriptors : proto,
+      constructor = getConstructor(
+        d = keys(d).reduce(descriptify, d),
+        defaults
+      ).value
+    ;
+    return (extending ?
+      (constructor[PROTOTYPE] = originalCreate(
+        typeof proto === 'function' ?
+          proto[PROTOTYPE] : proto, d)) :
+      defineProperties(constructor[PROTOTYPE], d)
+    )[CONSTRUCTOR];
+  }
+
   return {
-    Class: function (p, o) {
-      var
-        extending = o != null,
-        proto = extending ? o : p,
-        constructor = has.call(proto, CONSTRUCTOR) ?
-          proto[CONSTRUCTOR] :
-          (proto[CONSTRUCTOR] = function Class() {})
-      ;
-      return (constructor[PROTOTYPE] = extending ?
-        copyEnumerables(
-          create(
-            typeof p === 'function' ? p[PROTOTYPE] : p
-          ), o
-        ) :
-        proto
-      )[CONSTRUCTOR];
-    },
+    Class: getOwnPropertyDescriptor ?
+      defineProperties(
+        function Class(proto, descriptors) {
+          var
+            extending = descriptors != null,
+            d = extending ? descriptors : proto,
+            constructor = getConstructor(
+              d = keys(d).reduce(descriptify, d),
+              defaults
+            ).value
+          ;
+          return (extending ?
+            (constructor[PROTOTYPE] = originalCreate(
+              typeof proto === 'function' ?
+                proto[PROTOTYPE] : proto, d)) :
+            defineProperties(constructor[PROTOTYPE], d)
+          )[CONSTRUCTOR];
+        },
+        {
+          bound: {value: function bound(callback) {
+            return new Bound(callback);
+          }},
+          descriptor: {value: function descriptor(object) {
+            return new Descriptor(object);
+          }},
+          lazy: {value: function lazy(callback) {
+            return new Lazy(callback);
+          }}
+        }
+      ) :
+      function Class(proto, descriptors) {
+        var
+          extending = descriptors != null,
+          d = extending ? descriptors : proto,
+          constructor = getConstructor(d, Object)
+        ;
+        return (constructor[PROTOTYPE] = extending ?
+          copyEnumerables(create(
+            typeof proto === 'function' ?
+              proto[PROTOTYPE] : proto
+          ), d) : d
+        )[CONSTRUCTOR];
+      }
+    ,
     copy: copyEnumerables,
     create: function (p, o) {
       return o == null ? create(p) : set(o, p);
